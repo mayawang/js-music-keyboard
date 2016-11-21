355dd9c7f1ba9246e920e1f7fb8192b56efa60e1
Maya Wang <salome.neptune@gmail.com>
2016-11-21 15:46:47 -0800

    all keys sound on click. embeded in html. need to impletement the same feature in js

--- noise.js
+++ noise.js
@@ -2,13 +2,12 @@
 $(document).ready( function() {
 
   $('.body').click(function(event){
-    // $( ".note c" ).click();
-    // alert('click on key c' );
-    document.getElementsByTagName("cAudio").play();
-      // if (event.key == ' ') {
-      //   // $('#note c').removeClass();
-      //   $('#note c').addClass(nextColor());
-
+    // $('#cAudio').click(function(event){
+    //   document.getElementsByTagName("cAudio").play();
+    // });
+    // $('#dAudio').click(function(event){
+    //   document.getElementsByTagName("dAudio").play();
+    // });
   });
 
 
